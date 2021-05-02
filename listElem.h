dab9e45eadc1eaefc1eba14ea139bf33d0251936
AnibalPonte <49189960+AnibalPonte@users.noreply.github.com>
2021-05-02 23:26:58 +0100

    Add files via upload

---
+++ listElem.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "vaccination.h"
+
+typedef Vaccination ListElem; // Defining ListElem as KahootReport
+
+/* Function to print a listElem === KahootReport */
+void listElemPrint(ListElem elem);
