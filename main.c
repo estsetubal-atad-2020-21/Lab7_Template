d0eb1667d61d03a311731542426422c629d94dc1
AnibalPonte <49189960+AnibalPonte@users.noreply.github.com>
2021-05-02 23:56:53 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "input.h"
+
+int main() {
+
+    
+	printf("Nothing implemented yeat\n");
+
+    
+    return EXIT_SUCCESS;
+}
